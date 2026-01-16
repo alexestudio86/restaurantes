135f2a3d8325f1376bdcef3a8bafd7c6a936dd53
Alejandro R <alexestudio86@hotmail.com>
2026-01-16 10:27:25 -0600

    remote repository changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://alexestudio86.github.io',
+    base: '/restaurantes',
+    vite: {
+        resolve: {
+            alias: {
+                "@": "/src",
+                "@components": "/src/components",
+                "@layouts": "/src/layouts",
+                "@assets": "/src/assets",
+                "@styles": "/src/styles",
+            },
+        },
+    },
+});
