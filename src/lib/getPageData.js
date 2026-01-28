d6f01cbbac42a256834ed838169feb8df22e796b
Alejandro R <alexestudio86@gmail.com>
2026-01-27 20:27:56 -0600

    posts style begining

--- src/lib/getPageData.js
+++ src/lib/getPageData.js
@@ -4,7 +4,7 @@ export const getPageData = async() => {
         const [blog, posts, pages] = await Promise.all(
             [
                 fetch(`https://www.googleapis.com/blogger/v3/blogs/${import.meta.env.BLOG_ID}?key=${import.meta.env.API_KEY}`).then(r => r.json()),
-                fetch(`https://www.googleapis.com/blogger/v3/blogs/${import.meta.env.BLOG_ID}/posts?key=${import.meta.env.API_KEY}&fetchImages=true&maxResults=50&fields=items(id,title,images,labels,content,url)&nextPageToken`).then(r => r.json()),
+                fetch(`https://www.googleapis.com/blogger/v3/blogs/${import.meta.env.BLOG_ID}/posts?key=${import.meta.env.API_KEY}&fetchImages=true&maxResults=50&fields=items(id,title,labels,content,images)&nextPageToken`).then(r => r.json()),
                 fetch(`https://www.googleapis.com/blogger/v3/blogs/${import.meta.env.BLOG_ID}/pages?key=${import.meta.env.API_KEY}`).then(r => r.json())
             ]
         );
